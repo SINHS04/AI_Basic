180ac634b5a15a9b88cfe848ec9b2abf22e9ca22
SINHS04 <whruddn1013@naver.com>
2023-09-18 14:47:41 +0900

    commit

--- Linear_Regression/Multi_Variable/Linear_Regression_with_Mutil_Variable_1.py
+++ Linear_Regression/Multi_Variable/Linear_Regression_with_Mutil_Variable_1.py
@@ -1,46 +1,51 @@
 # A program to find a closest x where f(x) equals to target
-# f(x) = x * x
+# f(x) = x * x + 2
 
 import numpy as np
 import sys
 
 
 def func(x):
-    return x * x
+    return x**2 + 2
 
 
-def derivative(x):
-    return 2 * x
-
-
+min_range = -2
+max_range = 2
 alpha = 0.0000001
 attempt = 100000
 err = sys.float_info.epsilon
 
 data_size = 10000
-data = np.array([func(x) for x in np.arange(-2, 2, 4/data_size)])
-print(data)
-print(data[int(data_size/2)])
+x_data = np.arange(min_range, max_range, (max_range - min_range)/data_size)
+data = np.array([func(x) for x in x_data])
+
 a_now = 0
 a_pre = a_now
+b_now = 0
+b_pre = b_now
 
 
-def loss_derivative(a):
-    return np.sum(np.array([np.sqrt(2 * (a - 1) * np.power(x, 4)) for x in np.arange(-2, 2, 4/data_size)]))
+def loss_derivative_a(a, b):
+    return np.sum(np.array([2 * (x**2 * (a - 1) + b - 2) * x**2 for x in x_data]))
 
 
-def loss(a):
-    return np.sum(np.array([np.sqrt(np.power(x*x*(1-a), 2)) for x in np.arange(-2, 2, 4/data_size)]))
+def loss_derivative_b(a, b):
+    return np.sum(np.array([2 * (x**2 * (a - 1) + b - 2) for x in x_data]))
+
+
+def loss(a, b):
+    return np.sum(np.array([(x**2 * (a - 1) + 2 - b)**2 for x in x_data]))
 
 
 for i in range(attempt):
-    minus = alpha * loss_derivative(a_pre)
-    a_now = a_pre - minus
+    a_now = a_pre - alpha * loss_derivative_a(a_pre, b_pre)
+    b_now = b_pre - alpha * loss_derivative_b(a_pre, b_pre)
     a_pre = a_now
-    y = loss(a_now)
+    b_pre = b_now
+    y = loss(a_now, b_now)
     if np.abs(y) < err:
         break
     
-    print("attempt {} : ({}, {}, {})".format(i, a_now, y, minus))
+    print("attempt {} : (({}, {}), {})".format(i, a_now, b_now, y))
 
-print("answer : ({}, {})".format(a_now, func(a_now)))
+print("answer : ({}, {})".format(a_now, b_now))
