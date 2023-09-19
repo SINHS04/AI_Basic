eaec3c335f10b0f133e255052749e8f6dd0910e5
SINHS04 <whruddn1013@naver.com>
2023-09-19 11:29:03 +0900

    commit

--- Linear_Regression/Single_Variable/Linear_Regression_with_Single_Variable_3.py
+++ Linear_Regression/Single_Variable/Linear_Regression_with_Single_Variable_3.py
@@ -12,30 +12,31 @@ def func(x):
 
 min_range = 0
 max_range = np.pi
+data_range = max_range - min_range
 alpha = 0.000001
 attempt = 100000
 err = sys.float_info.epsilon
 
 data_size = 10000
-data = np.arange(min_range, max_range, (max_range - min_range)/data_size)
-a_now = 2
+data = np.array([func(x) for x in np.arange(min_range, max_range, data_range/data_size)])
+a_now = 0.9999
 a_pre = a_now
 
 
 def loss_derivative(a):
-    return np.sum(np.array([loss_derivative_function(a, x) if np.sign(loss_function(a, x)) == 1 else -loss_derivative_function(a, x) for x in data]))
+    return np.sum(np.array([loss_derivative_f(a, x) if np.sign(loss_f(a, x)) == 1 else -loss_derivative_f(a, x) for x in range(data_size)])) / data_size
 
 
 def loss(a):
-    return np.sum(np.array([np.abs(loss_function(a, x)) for x in data]))
+    return np.sum(np.array([np.abs(loss_f(a, x)) for x in range(data_size)])) / data_size
 
 
-def loss_derivative_function(a, x):
-    return x * np.cos(a*x)
+def loss_derivative_f(a, x):
+    return data_range * x * np.cos(a * data_range * x / data_size) / data_size
 
 
-def loss_function(a, x):
-    return np.sin(a*x) - np.sin(x)
+def loss_f(a, x):
+    return np.sin(a * data_range * x / data_size) - data[x]
 
 
 for i in range(attempt):
